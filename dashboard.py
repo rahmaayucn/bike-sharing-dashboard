13a7555d6833c4c2a511fd2c02dcd5cd4d6c3b23
Rahma Ayu <rahmaayu819@gmail.com>
2026-03-11 17:31:30 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 st.title("Bike Sharing Dashboard")
 
-data = pd.read_csv("../data/day.csv")
+data = pd.read_csv("main_data.csv")
 
 st.write("Dataset Preview")
 st.write(data.head())
 
 fig, ax = plt.subplots()
 sns.barplot(x="season", y="cnt", data=data, ax=ax)
+
 
 st.pyplot(fig)
